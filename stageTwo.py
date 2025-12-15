83619952195ed6986b7bc26e3d480f0b8ce6e877
Runbyzo <124527617+Runbyzo@users.noreply.github.com>
2025-12-15 13:14:30 +0300

    Update stageTwo.py

--- stageTwo.py
+++ stageTwo.py
@@ -1,42 +1,51 @@
-
-from stageOne import main as assemble_to_ir
+import argparse
+from stageOne import assemble_file
 
 
 def encode_instruction(inst):
-    #Кодирование одной IR-инструкции в байты УВМ
     result = bytearray()
 
     # Поле A (1 байт)
     result.append(inst["A"] & 0xFF)
 
-    # Поле B (size - 1 байт)
-    b_size = inst["size"] - 1
-    b_value = inst["B"]
+    # Определяем размер аргумента в зависимости от команды
+    if inst["mnemonic"] == "CONST":
+        b_value = inst["B"]
+        for i in range(3):
+            result.append((b_value >> (8 * i)) & 0xFF)
 
-    for i in reversed(range(b_size)):
-        result.append((b_value >> (8 * i)) & 0xFF)
+    elif inst["mnemonic"] == "LOAD":
+        b_value = inst["B"]
+        for i in range(2):
+            result.append((b_value >> (8 * i)) & 0xFF)
+
+    # STORE и != не имеют аргументов в бинарном виде
 
     return result
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Вариант 13, Этап 2"
-    )
+    parser = argparse.ArgumentParser(description="Вариант 13, Этап 2")
     parser.add_argument("source", help="исходный .asm файл")
     parser.add_argument("output", help="выходной бинарный файл")
+    parser.add_argument("--test", action="store_true", help="тестовый режим")
     args = parser.parse_args()
 
-    # IR
-    instructions = assemble_to_ir()
+    instructions = assemble_file(args.source, args.test)
+
+    if args.test:
+        print("\nМашинный код:")
 
     binary = bytearray()
 
-    # машинный код
     for inst in instructions:
-        binary.extend(encode_instruction(inst))
+        encoded = encode_instruction(inst)
+        binary.extend(encoded)
 
-    # Запись двоичного файла
+        if args.test:
+            hex_str = ' '.join([f'0x{b:02X}' for b in encoded])
+            print(f"{inst['mnemonic']} {inst['B']} -> {hex_str}")
+
     with open(args.output, "wb") as f:
         f.write(binary)
 
